5fa82aa3f91fa490eafd19757e45819d1f20acaa
Shubhika Singh <93629980+shubh1ka@users.noreply.github.com>
2023-01-17 17:06:57 +0530

    Add files via upload

---
+++ Inserting_a_record.py
@@ -0,0 +1,22 @@
+import _mysql_connector
+import sqlite3
+
+import mysql.connector
+
+conn = sqlite3.connect("newdatabase.db")
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="root123"
+)
+mycursor = mydb.cursor()
+#mycursor.execute("create database newdatabase")
+mycursor.execute("use newdatabase")
+#mycursor.execute("create table student1(name varchar(30) )")
+#mycursor.execute("create table student2(name varchar(30),address varchar(50),city varchar(70))")
+sql= "INSERT INTO student2 (name , address , city ) VALUES (%s,%s,%s)"
+val=("RIta","delhi","UP")
+mycursor.execute(sql,val)
+mydb.commit()
+print(mycursor.rowcount,"record inserted")
+mydb.close()
